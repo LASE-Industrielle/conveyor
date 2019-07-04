8e503c4c07c6bc2fc011e2f5f63f3f816dbc6935
ljmocic <ljubisa.mocic@gmail.com>
2019-07-04 10:17:43 +0200

    Implement pull to refresh

--- src/components/ConveyorDetailsForm.js
+++ src/components/ConveyorDetailsForm.js
@@ -1,25 +1,30 @@
 import React from "react";
-import {View, StyleSheet, Platform, ScrollView} from "react-native";
+import {
+  View,
+  StyleSheet,
+  Platform,
+  Dimensions,
+  ScrollView
+} from "react-native";
 import ConveyorDetailsFormRow from "../components/ConveyorDetailsFormRow";
 import ConveyorDetailsFormHeader from "../components/ConveyorDetailsFormHeader";
-import {elevationShadowStyle} from "../Styles";
+import { elevationShadowStyle } from "../Styles";
 
 const styles = StyleSheet.create({
   line: {
     marginHorizontal: 15,
     borderBottomColor: "#E8E6EA",
-    borderBottomWidth: 0.4,
+    borderBottomWidth: 0.4
   },
   formText: {
     fontFamily: "HelveticaNeue"
   }
 });
 
-const ConveyorDetailsForm = ({conveyor}) => {
+const ConveyorDetailsForm = ({ conveyor }) => {
   return (
     <View
       style={{
-        flex: 4,
         zIndex: 2,
         //paddingVertical: 30,
         paddingHorizontal: 5,
@@ -27,111 +32,113 @@ const ConveyorDetailsForm = ({conveyor}) => {
         borderRadius: 6,
         margin: 16,
         backgroundColor: "white",
-        ...elevationShadowStyle(2),
+        ...elevationShadowStyle(2)
       }}
     >
-      <ScrollView style={{
-        //flex: 4,
-        zIndex: 2,
-        paddingVertical: 30,
-        //paddingHorizontal: 5,
-        //justifyContent: "space-between",
-        //borderRadius: 6,
-        //margin: 16,
-        //marginTop: Platform.OS === 'ios' ? 102 : 0,
-        backgroundColor: "transparent",
-        //...elevationShadowStyle(2),
-        //marginHorizontal: 15,
-        //height: 200
-      }}>
-      <ConveyorDetailsFormHeader item1="JOB: #0001243" />
-      <View style={styles.line} />
+      <View
+        style={{
+          //flex: 4,
+          zIndex: 2,
+          paddingVertical: 30,
+          //paddingHorizontal: 5,
+          //justifyContent: "space-between",
+          //borderRadius: 6,
+          //margin: 16,
+          //marginTop: Platform.OS === 'ios' ? 102 : 0,
+          backgroundColor: "transparent"
+          //...elevationShadowStyle(2),
+          //marginHorizontal: 15,
+          //height: 200
+        }}
+      >
+        <ConveyorDetailsFormHeader item1="JOB: #0001243" />
+        <View style={styles.line} />
         <ConveyorDetailsFormRow
-            title="Customer Name"
-            item1={conveyor.customer.name}
-            item2=""
-        />
-      <View style={styles.line} />
-        <ConveyorDetailsFormRow
-            title="Material Name"
-            item1={conveyor.material.name}
-            item2=""
+          title="Customer Name"
+          item1={conveyor.customer.name}
+          item2=""
         />
         <View style={styles.line} />
         <ConveyorDetailsFormRow
-            title="Material Density"
-            item1="1.866"
-            item2={"t\u00B3/s"}
+          title="Material Name"
+          item1={conveyor.material.name}
+          item2=""
         />
         <View style={styles.line} />
         <ConveyorDetailsFormRow
-            title="Conveyor Speed"
-            item1={conveyor.latest_measurement.conveyor_speed}
-            item2={"m/s"}
-        />
-      <View style={styles.line} />
-      <ConveyorDetailsFormRow title="Belt Speed" item1="0.1" item2="m/s" />
-        <View style={styles.line} />
-        <ConveyorDetailsFormRow
-            title="Area"
-            item1={conveyor.latest_measurement.calculated_area}
-            item2={"m\u00B2"}
+          title="Material Density"
+          item1="1.866"
+          item2={"t\u00B3/s"}
         />
         <View style={styles.line} />
         <ConveyorDetailsFormRow
-            title="Total Area"
-            item1="0.363"
-            item2={"m\u00B2"}
+          title="Conveyor Speed"
+          item1={conveyor.latest_measurement.conveyor_speed}
+          item2={"m/s"}
+        />
+        <View style={styles.line} />
+        <ConveyorDetailsFormRow title="Belt Speed" item1="0.1" item2="m/s" />
+        <View style={styles.line} />
+        <ConveyorDetailsFormRow
+          title="Area"
+          item1={conveyor.latest_measurement.calculated_area}
+          item2={"m\u00B2"}
         />
         <View style={styles.line} />
         <ConveyorDetailsFormRow
-            title="Volume Sum"
-            item1={conveyor.latest_measurement.volume_sum}
-            item2={"m\u00B3/h"}
+          title="Total Area"
+          item1="0.363"
+          item2={"m\u00B2"}
         />
         <View style={styles.line} />
         <ConveyorDetailsFormRow
-            title="Volume Stream"
-            item1="1434.35"
-            item2={"m\u00B3/h"}
-        />
-      <View style={styles.line} />
-        <ConveyorDetailsFormRow
-            title="Volume Flow Rate"
-            item1={conveyor.latest_measurement.avg_volume_flow}
-            item2={"m\u00B3/s"}
-        />
-      <View style={styles.line} />
-      <ConveyorDetailsFormRow
-        title="Target Volume"
-        item1={conveyor.latest_measurement.limit_volume_sum}
-        item2={"m\u00B3"}
-      />
-        <View style={styles.line} />
-        <ConveyorDetailsFormRow
-            title="Mass Sum"
-            item1={conveyor.latest_measurement.mass_sum}
-            item2={"t"}
+          title="Volume Sum"
+          item1={conveyor.latest_measurement.volume_sum}
+          item2={"m\u00B3/h"}
         />
         <View style={styles.line} />
         <ConveyorDetailsFormRow
-            title="Mass Stream"
-            item1={conveyor.latest_measurement.avg_mass_flow}
-            item2={"t/h"}
+          title="Volume Stream"
+          item1="1434.35"
+          item2={"m\u00B3/h"}
         />
         <View style={styles.line} />
         <ConveyorDetailsFormRow
-            title="Cons. Deviation"
-            item1="66"
-            item2={"mm"}
+          title="Volume Flow Rate"
+          item1={conveyor.latest_measurement.avg_volume_flow}
+          item2={"m\u00B3/s"}
         />
         <View style={styles.line} />
         <ConveyorDetailsFormRow
-            title="Count Valid Pts"
-            item1={conveyor.latest_measurement.valid_points}
-            item2={"%"}
+          title="Target Volume"
+          item1={conveyor.latest_measurement.limit_volume_sum}
+          item2={"m\u00B3"}
         />
-      </ScrollView>
+        <View style={styles.line} />
+        <ConveyorDetailsFormRow
+          title="Mass Sum"
+          item1={conveyor.latest_measurement.mass_sum}
+          item2={"t"}
+        />
+        <View style={styles.line} />
+        <ConveyorDetailsFormRow
+          title="Mass Stream"
+          item1={conveyor.latest_measurement.avg_mass_flow}
+          item2={"t/h"}
+        />
+        <View style={styles.line} />
+        <ConveyorDetailsFormRow
+          title="Cons. Deviation"
+          item1="66"
+          item2={"mm"}
+        />
+        <View style={styles.line} />
+        <ConveyorDetailsFormRow
+          title="Count Valid Pts"
+          item1={conveyor.latest_measurement.valid_points}
+          item2={"%"}
+        />
+      </View>
     </View>
   );
 };
--- src/screens/ConveyorDetailsScreen.js
+++ src/screens/ConveyorDetailsScreen.js
@@ -1,160 +1,141 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-import {Platform, Text, TouchableOpacity, View, ScrollView} from "react-native";
+import {
+  Platform,
+  Text,
+  TouchableOpacity,
+  View,
+  ScrollView,
+  RefreshControl
+} from "react-native";
 import ConveyorProgresSvg from "../components/ConveyorProgressSvg";
 import ConveyorDetailsForm from "../components/ConveyorDetailsForm";
-import {elevationShadowStyle} from "../Styles";
+import { elevationShadowStyle } from "../Styles";
 import LinearGradient from "react-native-linear-gradient";
 import VolumeStreamComponent from "../components/VolumeStreamComponent";
 import GraphComponent from "../components/GraphComponent";
-import {useStateValue} from "../context/StateContext";
-import {getConveyorById} from "../services/ConveyorsService";
+import { useStateValue } from "../context/StateContext";
+import { getConveyorById } from "../services/ConveyorsService";
 
-const ConveyorDetailsScreen = ({navigation}) => {
-    const [{conveyor}, dispatch] = useStateValue();
-    const [percentage, setProgress] = useState(0);
+const ConveyorDetailsScreen = ({ navigation }) => {
+  const [{ conveyor }, dispatch] = useStateValue();
 
-    // useEffect(() => {
-    //     navigation.getParam('id', 1) == 1 ? navigation.setParams({title: 'Conveyor 1'}) : navigation.setParams({title: 'Conveyor 2'})
-    // }, []);
+  useEffect(() => {
+    getConveyorById(dispatch, navigation.getParam("id", ""));
+  }, []);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            if (percentage < 100) {
-                setProgress(percentage + 10);
-            }
-        }, 100);
-        return () => clearInterval(interval);
-    });
+  const reload = () => {
+    getConveyorById(dispatch, navigation.getParam("id", ""));
+  };
 
-    useEffect(() => {
-        getConveyorById(dispatch, navigation.getParam('id', ''));
-    }, []);
-
-    return (
-        <View
-            style={{
-                flex: 1,
-                flexDirection: "column",
-                //backgroundColor: "#F2F2F2",
-                fontFamily: "HelveticaNeue"
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "column",
+        //backgroundColor: "#F2F2F2",
+        fontFamily: "HelveticaNeue"
+      }}
+    >
+      <View
+        style={{
+          position: "absolute",
+          top: Platform.OS === "ios" ? 0 : 32,
+          zIndex: 1,
+          backgroundColor: "#F2F2F2",
+          flex: 1,
+          width: "100%",
+          height: "100%"
+        }}
+      >
+        {Platform.OS === "ios" ? (
+          <LinearGradient
+            style={{ height: 134 }}
+            colors={["#84CFA8", "#539A88"]}
+          />
+        ) : null}
+      </View>
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={conveyor.loading}
+            onRefresh={() => {
+              reload();
             }}
+          />
+        }
+        stickyHeaderIndices={[0]}
+        showsVerticalScrollIndicator={false}
+        style={{
+          flex: 4,
+          zIndex: 2,
+          //paddingVertical: 15,
+          //paddingHorizontal: 5,
+          //justifyContent: "space-between",
+          //borderRadius: 6,
+          //margin: 16,
+          marginTop: Platform.OS === "ios" ? 102 : 0,
+          backgroundColor: "transparent"
+          //...elevationShadowStyle(2),
+          //marginHorizontal: 15,
+        }}
+      >
+        <View
+          style={{
+            paddingHorizontal: 5,
+            justifyContent: "center",
+            flexDirection: "column",
+            borderRadius: 6,
+            marginHorizontal: 15,
+            marginTop: 15,
+            backgroundColor: "white",
+            ...elevationShadowStyle(2),
+            marginBottom: 4,
+            paddingVertical: 15,
+            //paddingLeft:
+            alignItems: "center"
+          }}
         >
-            <View
-                style={{
-                    position: "absolute",
-                    top: Platform.OS === 'ios' ? 0 : 32,
-                    zIndex: 1,
-                    backgroundColor: "#F2F2F2",
-                    flex: 1,
-                    width: "100%",
-                    height: "100%"
-                }}
-            >
-                {Platform.OS === 'ios' ?
-                    <LinearGradient style={{height: 134}}
-                                    colors={["#84CFA8", "#539A88"]}
-                    />
-                    :
-                    null
-                }
-            </View>
-            <View style={{
-                flex: 4,
-                zIndex: 2,
-                //paddingVertical: 15,
-                //paddingHorizontal: 5,
-                //justifyContent: "space-between",
-                //borderRadius: 6,
-                //margin: 16,
-                marginTop: Platform.OS === 'ios' ? 102 : 0,
-                backgroundColor: "transparent",
-                //...elevationShadowStyle(2),
-                //marginHorizontal: 15,
-
-            }}>
-                <View
-                    style={{
-                        paddingHorizontal: 5,
-                        justifyContent: "center",
-                        flexDirection: "column",
-                        borderRadius: 6,
-                        marginHorizontal: 15,
-                        //marginTop: 15,
-                        backgroundColor: "white",
-                        ...elevationShadowStyle(2),
-                        marginBottom: 4,
-                        paddingVertical: 15,
-                        //paddingLeft:
-                        alignItems: 'center'
-                    }}
-                >
-                    <VolumeStreamComponent
-                        percentage={50}
-                        upperLimit={conveyor.details.latest_measurement.upper_limit_flow}
-                        lowerLimit={conveyor.details.latest_measurement.lower_limit_flow}
-                    />
-                </View>
-                    <ConveyorDetailsForm conveyor={conveyor.details}/>
-                <View
-                    style={{
-                        flex: 1.25,
-                        justifyContent: "center",
-                        flexDirection: "column",
-                        //marginVertical: 1,
-                        zIndex: 2,
-
-                    }}
-                >
-                    <View
-                        style={{
-                            alignItems: "center",
-                            flex: 1,
-                            backgroundColor: "white",
-                            shadowColor: "#000000",
-                            justifyContent: "center",
-                            flexDirection: "column",
-                            paddingVertical: 20,
-
-                        }}
-                    >
-                        <ConveyorProgresSvg percentage={percentage.toString()}/>
-                    </View>
-                </View>
-                <View
-                    style={{
-                        backgroundColor: "white",
-                        flex: 1.11,
-                        justifyContent: "center",
-                        flexDirection: "column",
-                        zIndex: 2,
-                    }}
-                >
-                    <TouchableOpacity
-                        style={{
-                            marginVertical: 26,
-                            marginHorizontal: 16,
-                            paddingVertical: Platform.OS === 'ios' ? 0 : 17,
-                            alignItems: "center",
-                            flex: Platform.OS === 'ios' ? 0.7 : 1,
-                            backgroundColor: "#02A04E",
-                            justifyContent: "center",
-                            flexDirection: "column",
-                            borderRadius: 5,
-                            ...elevationShadowStyle(2),
-                            marginTop: 0
-                        }}
-                        onPress={() => {
-                            navigation.navigate("ScannersAnalytic");
-                        }}
-                    >
-                        <Text style={{color: "white", fontFamily: "HelveticaNeue"}}>ANALYTICS
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
+          <VolumeStreamComponent
+            percentage={50}
+            upperLimit={conveyor.details.latest_measurement.upper_limit_flow}
+            lowerLimit={conveyor.details.latest_measurement.lower_limit_flow}
+          />
         </View>
-    );
+        <ConveyorDetailsForm conveyor={conveyor.details} />
+      </ScrollView>
+      <View
+        style={{
+          backgroundColor: "white",
+          flex: 0.17,
+          justifyContent: "center",
+          flexDirection: "column",
+          zIndex: 2
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            margin: 22,
+            paddingVertical: Platform.OS === "ios" ? 0 : 5,
+            alignItems: "center",
+            flex: Platform.OS === "ios" ? 0.7 : 0.8,
+            backgroundColor: "#02A04E",
+            justifyContent: "center",
+            flexDirection: "column",
+            borderRadius: 5,
+            ...elevationShadowStyle(2)
+          }}
+          onPress={() => {
+            navigation.navigate("ScannersAnalytic");
+          }}
+        >
+          <Text style={{ color: "white", fontFamily: "HelveticaNeue" }}>
+            ANALYTICS
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
 
 export default ConveyorDetailsScreen;
