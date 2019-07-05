3acfae5aefa6f15c13744ea5fa82200768b0c0e9
Nikola Lisicic <nikola.lisicic@Nikolas-MacBook-Pro.local>
2019-07-05 16:22:22 +0200

    Improve user experience on graphs

--- src/components/GraphComponent.js
+++ src/components/GraphComponent.js
@@ -1,60 +1,57 @@
-import React from "react";
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from "react";
+import {ActivityIndicator, Text, View} from 'react-native';
 import {VictoryAxis, VictoryChart, VictoryLine, VictoryTheme} from "victory-native";
 
 const GraphComponent = props => {
 
-    const {label,lineColor, data, value, units,loading} = props;
-    let values = [];
-    data.map((item,index) => {
-        values.push({x:index,y:item});
-    })
+    const {label, lineColor, data, ticks, value, units} = props;
+    const [loader, setLoader] = useState(false);
 
-    let maxValue = Math.max(...data)
-    let minValue = Math.min(...data)
-    let midValue = (maxValue + minValue)/2;
-    const generateConstantTicks = value => [value-2,value-1,value,value+1,value+2];
-    const generateAvgTicks = (minValue,midValue,maxValue) => [minValue,(minValue+midValue)/2,midValue,(midValue+maxValue)/2,maxValue];
-    const ticks = (maxValue-minValue===0)?generateConstantTicks(minValue):generateAvgTicks(minValue,midValue,maxValue);
-
-
-
-
-
+    // hack chart drawing to be done in background
+    useEffect(() => {
+        (async () => {
+            setLoader(true)
+        })()
+    }, []);
 
     return (
         <View>
-            <Text style={{paddingLeft:15, paddingTop:20}}>{label}</Text>
-            <Text style={{fontSize:16, color: lineColor,paddingLeft:15}}>{value} {units}</Text>
-            {values && <VictoryChart domainPadding={20} padding={{left: 55, right: 50, bottom: 20, top: 20}} height={300} theme={VictoryTheme.material}>
-                {!loading && <VictoryAxis
-                    dependentAxis
-                    orientation="left"
-                    tickValues={ticks}
-                     tickFormat={(t) => ~~(t)}
-                    style={{
-                        axis: {stroke: "#F2F2F2"},
-                        ticks: {stroke: "#F2F2F2"},
-                        grid: {stroke: "#F2F2F2"},
-                    }}/>
-                }
-                <VictoryAxis
-                    orientation='bottom'
-                    tickValues={values.map(item => item.x)}
-                    tickFormat={values.map(item => '')}
-                    style={{
-                        axis: {stroke: "#F2F2F2"},
-                        ticks: {stroke: "none"},
-                        grid: {stroke: "none"},
-                    }}
-                />
-                {!loading &&
-                <VictoryLine
-                    style={{data: {stroke: lineColor}}}
-                    data={values}
-                    interpolation='natural'
-                />}
-            </VictoryChart>}
+            <Text style={{paddingLeft: 15, paddingTop: 20}}>{label}</Text>
+            <Text style={{fontSize: 16, color: lineColor, paddingLeft: 15}}>{value} {units}</Text>
+            {data && loader ?
+                <VictoryChart domainPadding={20} padding={{left: 55, right: 50, bottom: 20, top: 20}} height={300}
+                              theme={VictoryTheme.material}>
+                    <VictoryAxis
+                        dependentAxis
+                        orientation="left"
+                        tickValues={ticks}
+                        tickFormat={(t) => ~~(t)}
+                        style={{
+                            axis: {stroke: "#F2F2F2"},
+                            ticks: {stroke: "#F2F2F2"},
+                            grid: {stroke: "#F2F2F2"},
+                        }}/>
+
+                    <VictoryAxis
+                        orientation='bottom'
+                        tickValues={data.map(item => item.x)}
+                        tickFormat={data.map(item => '')}
+                        style={{
+                            axis: {stroke: "#F2F2F2"},
+                            ticks: {stroke: "none"},
+                            grid: {stroke: "none"},
+                        }}
+                    />
+
+                    <VictoryLine
+                        style={{data: {stroke: lineColor}}}
+                        data={data}
+                        interpolation='natural'
+                    />
+                </VictoryChart> :
+                <View style={{height: 300, alignItems: 'center', justifyContent: 'center'}}>
+                    <ActivityIndicator animating={true}/>
+                </View>}
         </View>
     );
 
--- src/screens/ScannersAnalyticScreen.js
+++ src/screens/ScannersAnalyticScreen.js
@@ -1,33 +1,70 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {getConveyors} from "../services/ConveyorsService";
 import {useStateValue} from "../context/StateContext";
 import {elevationShadowStyle} from "../Styles";
-import {Platform, ScrollView, Text, View, RefreshControl} from "react-native";
-import GraphComponent from "../components/GraphComponent";
+import {Platform, RefreshControl, ScrollView, View} from "react-native";
 import ConveyorStatusForm from "../components/ConveyorStatusForm";
 import LinearGradient from "react-native-linear-gradient";
 import {blueGraph, orangeGraph, redGraph} from "../Colors";
+import GraphComponent from '../components/GraphComponent';
 
 const ScannersAnalyticScreen = props => {
-    const conveyorId = props.navigation.getParam("id");
+
+    const [volumeSumMeasurements, setVolumeSumMeasurements] = useState();
+    const [volumeSumMeasurementsTicks, setVolumeSumMeasurementsTicks] = useState();
+
+    const [volumeFlowMeasurements, setVolumeFlowMeasurements] = useState();
+    const [volumeFlowMeasurementsTicks, setVolumeFlowMeasurementsTicks] = useState();
+
+    const [conveyorSpeedMeasurements, setConveyorSpeedMeasurements] = useState();
+    const [conveyorSpeedMeasurementsTicks, setConveyorSpeedMeasurementsTicks] = useState();
+
+    const [volumeSum, setVolumeSum] = useState();
+    const [avgVolumeFlow, setAvgVolumeFlow] = useState();
+    const [conveyorSpeed, setConveyorSpeed] = useState();
+    const [scannerStatus, setScannerStatus] = useState();
 
     const [{conveyors}, dispatch] = useStateValue();
-    const latestMeasurement = conveyors.data[0].latest_measurement;
-    const chartData = conveyors.data[0].chart_data;
-    const {conveyor_speed, volume_sum, avg_volume_flow, scanner_status} = latestMeasurement;
-    const volumeSumMeasurements = chartData.volume_sum;
-    const volumeFlowMeasurements = chartData.volume_flow;
-    const conveyorSpeedMeasurements = chartData.conveyor_speed;
 
+    const createTicks = data => {
+        let maxValue = Math.max(...data);
+        let minValue = Math.min(...data);
+        let midValue = (maxValue + minValue) / 2;
+        const generateConstantTicks = value => [value - 2, value - 1, value, value + 1, value + 2];
+        const generateAvgTicks = (minValue, midValue, maxValue) => [minValue, (minValue + midValue) / 2, midValue, (midValue + maxValue) / 2, maxValue];
+        return (maxValue - minValue === 0) ? generateConstantTicks(minValue) : generateAvgTicks(minValue, midValue, maxValue);
+    };
+
+    const refreshData = () => {
+        getConveyors(dispatch);
+        const {latest_measurement: latestMeasurement, chart_data: chartData} = conveyors.data[0];
+
+        setVolumeSum(latestMeasurement.volume_sum);
+        setAvgVolumeFlow(latestMeasurement.avg_volume_flow);
+        setConveyorSpeed(latestMeasurement.conveyor_speed)
+        setScannerStatus(latestMeasurement.scanner_status);
+
+        const volumeSumMeasurements = chartData.volume_sum;
+        const volumeFlowMeasurements = chartData.volume_flow;
+        const conveyorSpeedMeasurements = chartData.conveyor_speed;
+
+        setVolumeSumMeasurementsTicks(createTicks(volumeSumMeasurements));
+        setVolumeSumMeasurements(volumeSumMeasurements.map((item, index) => ({x: index, y: item})));
+
+        setVolumeFlowMeasurementsTicks(createTicks(volumeFlowMeasurements));
+        setVolumeFlowMeasurements(volumeFlowMeasurements.map((item, index) => ({x: index, y: item})));
+
+        setConveyorSpeedMeasurementsTicks(createTicks(conveyorSpeedMeasurements));
+        setConveyorSpeedMeasurements(conveyorSpeedMeasurements.map((item, index) => ({x: index, y: item})));
+    };
 
     useEffect(() => {
-        getConveyors(dispatch);
-
+        refreshData()
     }, []);
 
     const onRefresh = () => {
-        getConveyors(dispatch);
-    }
+        refreshData()
+    };
 
     return (
         <View style={{backgroundColor: 'transparent', width: '100%', height: '100%'}}>
@@ -43,16 +80,16 @@ const ScannersAnalyticScreen = props => {
                 }}
             >
                 {Platform.OS === 'ios' &&
-                    <LinearGradient style={{height: 134}} colors={["#84CFA8", "#539A88"]}/>}
+                <LinearGradient style={{height: 134}} colors={["#84CFA8", "#539A88"]}/>}
             </View>
-            <ScrollView refreshControl={<RefreshControl refreshing={conveyors.loading} onRefresh={onRefresh}
+            <ScrollView refreshControl={<RefreshControl refreshing={false} onRefresh={onRefresh}
             />}
-                style={{
-                    zIndex: 2,
-                    marginTop: Platform.OS === 'ios' ? 102 : 0,
-                    backgroundColor: "transparent"
-                }}
-                >
+                        style={{
+                            zIndex: 2,
+                            marginTop: Platform.OS === 'ios' ? 102 : 0,
+                            backgroundColor: "transparent"
+                        }}
+            >
                 <View
                     pointerEvents="none"
                     style={{
@@ -61,22 +98,33 @@ const ScannersAnalyticScreen = props => {
                         flexDirection: "column",
                         borderRadius: 6,
                         marginHorizontal: 15,
-                        //marginTop: 15,
                         backgroundColor: "white",
                         ...elevationShadowStyle(2),
                         marginBottom: 18,
                     }}
                 >
 
-                    <GraphComponent lineColor={orangeGraph} loading={conveyors.loading} label={'Volume Sum'} data={volumeSumMeasurements} value={volume_sum} units={'dm\u00B3'}/>
-                    <GraphComponent lineColor={blueGraph} loading={conveyors.loading}  label={'Volume Flow Rate'} data={volumeFlowMeasurements} value={avg_volume_flow} units={'dm\u00B3/h'}/>
-                    <GraphComponent lineColor={redGraph} loading={conveyors.loading}  label={'Conveyor Speed'} data={conveyorSpeedMeasurements} value={conveyor_speed} units={'mm/s'}/>
+                    {volumeSumMeasurements &&
+                    <GraphComponent lineColor={orangeGraph} loading={false} label={'Volume Sum'}
+                                    data={volumeSumMeasurements} ticks={volumeSumMeasurementsTicks}
+                                    value={volumeSum}
+                                    units={'dm\u00B3'}/>}
 
+                    {volumeFlowMeasurements &&
+                    <GraphComponent lineColor={blueGraph} loading={false} label={'Volume Flow Rate'}
+                                    data={volumeFlowMeasurements} ticks={volumeFlowMeasurementsTicks}
+                                    value={avgVolumeFlow}
+                                    units={'dm\u00B3/h'}/>}
+
+                    {conveyorSpeedMeasurements &&
+                    <GraphComponent lineColor={redGraph} loading={false} label={'Conveyor Speed'}
+                                    data={conveyorSpeedMeasurements} ticks={conveyorSpeedMeasurementsTicks}
+                                    value={conveyorSpeed}
+                                    units={'mm/s'}/>}
                 </View>
-
             </ScrollView>
-            <View style={{height:60}}>
-                <ConveyorStatusForm status={scanner_status}/>
+            <View style={{height: 60}}>
+                <ConveyorStatusForm status={scannerStatus}/>
             </View>
 
         </View>
