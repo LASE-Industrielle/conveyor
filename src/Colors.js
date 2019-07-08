efde823dcc168d94d29281efef078743d216e14b
ljmocic <ljubisa.mocic@gmail.com>
2019-07-08 09:53:50 +0200

    Setup prettier

--- src/Colors.js
+++ src/Colors.js
@@ -1,25 +1,25 @@
-const primary = "#01A04E";
-const secondary = "#01A000";
-const primaryText = "#606060";
-const secondaryText = "#969696";
-const inactiveText = "#C5C5C5";
+const primary = '#01A04E';
+const secondary = '#01A000';
+const primaryText = '#606060';
+const secondaryText = '#969696';
+const inactiveText = '#C5C5C5';
 
-const iconColor = "#FFF";
-const greenIconColor = "#02A04E";
+const iconColor = '#FFF';
+const greenIconColor = '#02A04E';
 
-const blackTextColor = "#797979";
+const blackTextColor = '#797979';
 
-const orange = "#F1B950";
-const blue = "#3498DB";
-const red = "#F18382";
-const black = "#262626";
+const orange = '#F1B950';
+const blue = '#3498DB';
+const red = '#F18382';
+const black = '#262626';
 
-const bgColor = "#F2F2F2";
-const bgGradientStart = "#84CFA8";
-const bgGradientEnd = "#539A88";
+const bgColor = '#F2F2F2';
+const bgGradientStart = '#84CFA8';
+const bgGradientEnd = '#539A88';
 
-const statusColorGreen = "#6CC799";
-const statusColorRed = "#F19B93";
+const statusColorGreen = '#6CC799';
+const statusColorRed = '#F19B93';
 
 export {
   primary,
