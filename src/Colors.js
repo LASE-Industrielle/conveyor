a5a4b98dc4f954c0ca918ea727b77177f2279dca
ljmocic <ljubisa.mocic@gmail.com>
2019-07-09 08:54:18 +0200

    Grouping colors

--- src/Colors.js
+++ src/Colors.js
@@ -5,11 +5,11 @@ const secondaryText = '#969696';
 const inactiveText = '#C5C5C5';
 
 const white = '#FFFFFF';
+const blackTextColor = '#797979';
+const greyText = '#AAA9A9';
 
 const iconColor = '#FFF';
 const greenIconColor = '#02A04E';
-
-const blackTextColor = '#797979';
 
 const orange = '#F1B950';
 const blue = '#3498DB';
@@ -19,17 +19,13 @@ const black = '#262626';
 const bgColor = '#F2F2F2';
 const bgGradientStart = '#84CFA8';
 const bgGradientEnd = '#539A88';
+const bgGradientStartLogin = '#83CEA7';
 
 const statusColorGreen = '#6CC799';
 const statusColorRed = '#F19B93';
 
 const bottomBorder = '#3A7F78';
-
 const whiteBorder = '#E8E6EA';
-
-const greyText = '#AAA9A9';
-
-const bgGradientStartLogin = '#83CEA7';
 
 export {
   white,
